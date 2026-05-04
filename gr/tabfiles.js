060f90fda2b84ef2e43863d8da7712be781787e3
applane <86399847+applane@users.noreply.github.com>
2026-05-04 17:50:49 +0300

    add

---
+++ gr/tabfiles.js
@@ -0,0 +1,61 @@
+window.g_tabFiles = [
+  { band: "AC-DC", tabs: [
+    "Are you ready.gp5",
+    "The Jack.gp4"
+  ] },
+  { band: "Accept", tabs: [
+    "Balls To the Wall.gp3"
+  ] },
+  { band: "Alice Cooper", tabs: [
+    "Poison.gp4"
+  ] },
+  { band: "Bon Jovi", tabs: [
+    "Bad Medicine.gp4",
+    "Livin On A Prayer.gp4",
+    "Wanted Dead Or Alive.gp4"
+  ] },
+  { band: "Deep Purple", tabs: [
+    "Smoke On The Water.gp3"
+  ] },
+  { band: "Europe", tabs: [
+    "The Final Countdown.gp4"
+  ] },
+  { band: "Guns N' Roses", tabs: [
+    "Knockin' On Heaven's Door.gp3",
+    "Welcome To The Jungle.gp4"
+  ] },
+  { band: "Helloween", tabs: [
+    "A Tale That Wasn't Right.gpx",
+    "Future World.gp3",
+    "Heavy Metal.gp3"
+  ] },
+  { band: "Judas Priest", tabs: [
+    "Breaking The Law.gp3"
+  ] },
+  { band: "Kiss", tabs: [
+    "Lick It Up.gp3"
+  ] },
+  { band: "Lordi", tabs: [
+    "Hard rock hallelujah.gp5"
+  ] },
+  { band: "Manowar", tabs: [
+    "Brothers Of Metal.gp4",
+    "Fighting The World.gp3",
+    "Kings Of Metal.gp3",
+    "Number 1.gp4",
+    "The Gods Made Heavy Metal.gp4"
+  ] },
+  { band: "Megadeth", tabs: [
+    "Hanger 18.gp4",
+    "Peace Sells... But Whos's Buying.gp4",
+    "Symphony Of Destruction.gp4"
+  ] },
+  { band: "Ozzy", tabs: [
+    "Mama, I'm Coming Home.gp3",
+    "No More Tears.gp3"
+  ] },
+  { band: "ZZ Top", tabs: [
+    "Gimme All Your Lovin`.gp3",
+    "Sharp Dressed Man.gp4"
+  ] }
+];
